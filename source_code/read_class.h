ddb89bfe65e108477d58413fd29be5e58827659e
nemg98 <a.negm98.an@gmail.com>
2019-04-29 06:05:50 +0200

    include <string> in read_class

--- source_code/read_class.h
+++ source_code/read_class.h
@@ -1,5 +1,6 @@
 #ifndef READ_CLASS_H
 #define READ_CLASS_H
+#include <string>
 using namespace std;
 
 class read{
@@ -12,16 +13,18 @@ private:
   int len;
 
 public:
-  read(string Qname="*", string Rname="*", int pos=0, int len=0, int mapQ=255);
+  read(string Qname="*", string Rname="*", int flag=0, int pos=0, int len=0, int mapQ=255);
 
   void setQname(string);
   void setRname(string);
+  void setFlag(int);
   void setPos(int);
   void setMapQ(int);
   void setLen(int);
 
   string getQname();
   string getRname();
+  int getFlag();
   int getPos();
   int getMapQ();
   int getLen();
