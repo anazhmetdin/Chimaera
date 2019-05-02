5cc5b9fb01f41648f806ab8d943e65cb77a7d563
nemg98 <a.negm98.an@gmail.com>
2019-05-02 10:28:01 +0200

    edit hash_f.h to enable public acess ask the user wheathe to build to build a new hash_f or not

--- source_code/pipeline_supported_chim_loci.sh
+++ source_code/pipeline_supported_chim_loci.sh
@@ -5,12 +5,13 @@
 
 rm ./pipline_run/directed_reads.sam 2> ./pipline_run/no_files.txt
 rm ./pipline_run/filtered_chim_reads.sam 2> ./pipline_run/no_files.txt
+rm ./pipline_run/supported_loci.txt 2> ./pipline_run/no_files.txt
 
 ./add_field_direction.sh $1
-mv directed_reads.sam directed_reads1.sam
+mv directed_reads.sam directed_reads1.sam 2> ./pipline_run/no_files.txt
 awk 'BEGIN{FS=OFS="\t"}{print $0 OFS "SA:"}' directed_reads1.sam > directed_reads.sam
-mv directed_reads.sam ./pipline_run
-rm directed_reads1.sam
+mv directed_reads.sam ./pipline_run 2> ./pipline_run/no_files.txt
+rm directed_reads1.sam 2> ./pipline_run/no_files.txt
 echo done directed
 
 ./extract_chim_reads.sh ./pipline_run/directed_reads.sam -> ./pipline_run/filtered_chim_reads.sam
@@ -19,7 +20,24 @@ echo done chim
 ./extract_needed_fields.sh ./pipline_run/filtered_chim_reads.sam -> ./pipline_run/short_lines.sam
 echo done needed
 
+mv ./pipline_run/short_lines.sam ./pipline_run/short_lines1.sam 2> ./pipline_run/no_files.txt
+head -n 20000 ./pipline_run/short_lines1.sam > ./pipline_run/short_lines.sam
+rm ./pipline_run/short_lines1.sam 2> ./pipline_run/no_files.txt
 
-./build_hash_f.sh ./pipline_run/short_lines.sam
+echo Do you want to build a new hash_function?
+echo [y/n]?
+read option
+if [ $option == 'y' ]
+then
+  ./build_hash_f.sh ./pipline_run/short_lines.sam
+  rm ./sup_reads_hash_function.gperf 2> ./pipline_run/no_files.txt
 
-rm ./sup_reads_hash_function.gperf
+  sed '44d' hash_f.h > hash_f1.h
+  sed 's/private/public/' hash_f1.h > hash_f.h
+  rm ./hash_f1.h 2> ./pipline_run/no_files.txt
+fi
+
+
+g++ store_chi_reads.cpp linked.cpp table.cpp read_class.cpp node.cpp locus.cpp
+
+./a.out ./pipline_run/short_lines.sam
