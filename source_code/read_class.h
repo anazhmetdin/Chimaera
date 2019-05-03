248c595b95f0676b2f33a59ea1faa12535f2594c
nemg98 <a.negm98.an@gmail.com>
2019-05-03 02:12:18 +0200

    fix syntax errors

--- source_code/read_class.h
+++ source_code/read_class.h
@@ -20,22 +20,32 @@ private:
   int dir;
 
 public:
-  read(int dir=0,string Qname="*", int flag=0, string Rname="*", int pos=0, , int mapQ=255, string cigar="-", string Rnext="*", int Pnext=0, int len=0, string seq="*", string quality="*");
+  read(int dir=0,string Qname="*", int flag=0, string Rname="*", int pos=0, int mapQ=255, string cigar="-", string Rnext="*", int Pnext=0, int len=0, string seq="*", string quality="*");
 
-  void setQname(string);
-  void setRname(string);
-  void setFlag(int);
-  void setPos(int);
-  void setMapQ(int);
-  void setLen(int);
-  void setDir(int);
+  void setQname(string Qname);
+  void setFlag(int flag);
+  void setRname(string Rname);
+  void setPos(int pos);
+  void setMapQ(int mapQ);
+  void setCigar(string cigar);
+  void setRnext(string Rnext);
+  void setPnext(int Pnext);
+  void setLen(int len);
+  void setSeq(string seq);
+  void setQuality(string quality);
+  void setDir(int dir);
 
   string getQname();
-  string getRname();
   int getFlag();
+  string getRname();
   int getPos();
   int getMapQ();
+  string getCigar();
+  string getRnext();
+  int getPnext();
   int getLen();
+  string getSeq();
+  string getQuality();
   int getDir();
 
   string printRead();
